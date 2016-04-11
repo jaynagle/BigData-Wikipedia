57d3bdd825b473557157e6e6125f1258718d8297
Anwar <mail2anwarshaikh@gmail.com>
2016-04-11 02:24:56 -0400

    Testing JWPL library

---
+++ Parser/src/com/Test_JWPL.java
@@ -0,0 +1,31 @@
+package com;
+
+import de.tudarmstadt.ukp.wikipedia.api.DatabaseConfiguration;
+import de.tudarmstadt.ukp.wikipedia.api.WikiConstants.Language;
+import de.tudarmstadt.ukp.wikipedia.api.Wikipedia;
+import de.tudarmstadt.ukp.wikipedia.api.exception.WikiApiException;
+import de.tudarmstadt.ukp.wikipedia.api.exception.WikiInitializationException;
+
+public class Test_JWPL {
+
+	public static void main(String[] args) throws WikiApiException {
+		// TODO Auto-generated method stub
+		
+		    DatabaseConfiguration dbConfig = new DatabaseConfiguration();
+	        dbConfig.setHost("localhost");
+	        dbConfig.setDatabase("wikipedia_jwpl");
+	        dbConfig.setUser("root");
+	        dbConfig.setPassword("");
+	        dbConfig.setLanguage(Language.english);
+	        
+	        // Create a new German wikipedia.
+	        Wikipedia wiki = new Wikipedia(dbConfig);
+	        
+	        // Get the page with title "Hello world".
+	        // May throw an exception, if the page does not exist.
+	        de.tudarmstadt.ukp.wikipedia.api.Page page = wiki.getPage("Hello world");
+	        System.out.println(page.getText());
+
+	}
+
+}
