7d297df563f04e3996e546a211dc38f82fd1a817
Jay Nagle <naglejay@gmail.com>
2016-04-24 12:52:36 -0400

    Semantic Role Labeling script

---
+++ TextAnalysis/LabelRoles.py
@@ -0,0 +1,55 @@
+import os
+import nltk
+from nltk.corpus import wordnet as wn
+from practnlptools.tools import Annotator
+import csv
+import traceback
+from nltk import stem
+import re 
+
+path = "C:/Users/hp/Desktop/bigdata/txt/"
+data = []
+desired_verbs = ['caused', 'preceeded', 'because', 'due to', 'as a result', 'therefore', 'as a consequence', 'thus', 'because of', 'since', 'consequently', 'causes', 'results','resulted', 'affected', 'increased', 'decreaed', 'rose', 'rise']
+
+stemmed_desired_verbs=[]
+stemmer=stem.snowball.EnglishStemmer()
+
+for word in desired_verbs:
+    stemmed_desired_verbs.append(stemmer.stem(word))
+   
+annotator=Annotator()
+
+f = open('out.csv', 'wt')
+csv.register_dialect('lineterminator',lineterminator='\n')
+writer = csv.writer(f, dialect = csv.get_dialect('lineterminator'))
+writer.writerow( ('A0', 'A1', 'V', 'fileName'))
+for filename in os.listdir(path):
+    print 'reading', filename
+    text_file = open(path + filename,"r")
+    file_content = text_file.readlines()
+    data.append((filename,file_content))
+    text_file.close()
+    
+    for t1 in file_content:
+        for text in t1.split("."):
+            text.strip()
+            if text:
+                try:
+                    x = annotator.getAnnotations(text)['srl']
+                    for x_tuple in x:
+                        a0 = None
+                        a1 = None
+                        v = None 
+                        for item in x_tuple:
+                            if item == 'A0':
+                                a0 = x_tuple['A0']
+                            if item == 'A1':
+                                a1 = x_tuple['A1']
+                            if item == 'V':
+                                if stemmer.stem(x_tuple['V']) in stemmed_desired_verbs:
+                                    v = x_tuple['V']
+                        if (a0 is not None and a1 is not None and v is not None):
+                            writer.writerow( (x_tuple['A0'], x_tuple['A1'], x_tuple['V'], filename))
+                except Exception as e:
+                    traceback.print_exc()
+    f.flush()                
