087721c3b16f96c464c88505b27b379bf9c2a60e
vinayvrv <vernekarvinay@gmail.com>
2016-05-02 22:32:06 -0400

    Visualize Similarity of events
    This Code helps to see the similarity between events based the WUP similarity values

---
+++ TextAnalysis/Similarity Matrix heatmap code.R
@@ -0,0 +1,12 @@
+# This program is designed to see the WUP Similarity matrix as a heatmap
+
+
+install.packages("corrplot")
+library(corrplot)
+
+setwd("C:\\Second Sem\\Big Data and Analytics")
+
+D=read.csv("datamatrix.csv")
+head(D)
+heatmap(as.matrix(D[-1,-1]))
+
